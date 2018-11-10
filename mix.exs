8dc10dcc38f464e08194044106e72093ec193a23
jonnypower <me@jonnypower.com>
2018-11-10 14:20:44 -0800

    feat: Create application with dynamic supervisor for client workers

--- mix.exs
+++ mix.exs
@@ -7,20 +7,30 @@ defmodule HLDSRcon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "HLDSRcon",
+      source_url: "https://github.com/JonnyPower/hlds_rcon",
+      docs: [
+        main: "HLDSRcon",
+        extras: ["README.md"]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {HLDSRcon.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
