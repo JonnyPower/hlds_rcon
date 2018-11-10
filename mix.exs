6a2b9d5e947e1a0b4ad040af1a3b2e2d272d78a8
jonnypower <me@jonnypower.com>
2018-11-09 21:58:55 -0800

    feat: Remove unnecessary application module

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule HLDSRcon.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {HLDSRcon.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
